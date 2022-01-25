686da97a57109259c7966182e7f690c128db65eb
Sophiat-O <lilsylph@gmail.com>
2022-01-25 21:17:05 +0100

    New java project

--- src/main/java/JSONConvertXML.java
+++ src/main/java/JSONConvertXML.java
@@ -18,12 +18,12 @@ public class JSONConvertXML {
     public static void main(String[] args) {
 
         try {
-            File fXmlFile = new File("src/main/java/map.osm");
+            File fXmlFile = new File("src/main/java/book.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
             JSONObject jsonObject = new JSONObject();
-            JSONArray jsonArrays = new JSONArray();
+            JSONArray jsonArray = new JSONArray();
 
 
             doc.getDocumentElement().normalize();
@@ -41,58 +41,38 @@ public class JSONConvertXML {
             for(int i = 1; i < list.getLength(); i++) {
                 Node nList = list.item(i);
                 Node nchild = nList.getChildNodes().item(i);
-                Element eElement =(Element) nList;
-                NodeList eList = doc.getElementsByTagName(eElement.getNodeName());
                 Map elementMap = new LinkedHashMap();
                 Map oneElement = new LinkedHashMap();
-                JSONArray jsonArray = new JSONArray();
-                //System.out.println(eElement.getNodeName());
 
+                for (int j = 0; j < nList.getAttributes().getLength(); j++) {
 
+                    elementMap.put(nList.getAttributes().item(j).getNodeName(), nList.getAttributes().item(j).getNodeValue());
+                }
+                if(nList.getAttributes().getLength() > 1) {
 
-                if(eList.getLength()==1){
-                    for (int j = 0; j < nList.getAttributes().getLength(); j++) {
+                    jsonArray.add(elementMap);
 
-                        oneElement.put(nList.getAttributes().item(j).getNodeName(), nList.getAttributes().item(j).getNodeValue());
-
+                    if (nList.hasChildNodes()) {
+                        books.put(nList.getChildNodes().item(i), jsonArray);
+                    } else {
+                        books.put(nList.getNodeName(), jsonArray);
                     }
-
-                    books.put(nList.getNodeName(),oneElement);
-
-
-
                 }else {
-                    JSONArray nArray = new JSONArray();
-                    if(nList.getAttributes().getLength() > 1) {
-                        for (int j = 0; j < nList.getAttributes().getLength(); j++) {
-
-                            elementMap.put(nList.getAttributes().item(j).getNodeName(), nList.getAttributes().item(j).getNodeValue());
-                        }
-                        nArray.add(elementMap);
-                        jsonArray.add(nArray);
-                        jsonArrays.add(jsonArray);
-                        if(nList.hasChildNodes() && nchild !=null) {
-                            books.put(nchild.getNodeName(), jsonArrays);
-                        }
-                        else{
-                            books.put(nList.getNodeName(),jsonArrays);
-                        }
-                    }else {
-                        if(nList.hasChildNodes()) {
-                            books.put(nList.getChildNodes().item(i).getNodeName(), elementMap);
-                        }
-                        else{
-                            books.put(nList.getNodeName(),elementMap);
-                        }
+                    if(nList.hasChildNodes()) {
+                        books.put(nList.getChildNodes().item(i), elementMap);
+                    }
+                    else{
+                        books.put(nList.getNodeName(),elementMap);
                     }
                 }
+
 
 
             }
 
             jsonObject.put(rootNode,books);
 
-            PrintWriter pw = new PrintWriter("src/main/java/maps.json");
+            PrintWriter pw = new PrintWriter("src/main/java/map.json");
             pw.write(jsonObject.toJSONString());
             pw.flush();
             pw.close();
