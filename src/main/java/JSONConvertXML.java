9208d82662fac391626d4ea740330859417063dc
Sophiat-O <lilsylph@gmail.com>
2022-01-25 13:41:55 +0100

    New java project

--- src/main/java/JSONConvertXML.java
+++ src/main/java/JSONConvertXML.java
@@ -1,6 +1,7 @@
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -27,17 +28,27 @@ public class JSONConvertXML {
             String rootNode = doc.getDocumentElement().getNodeName();
             NodeList list = doc.getElementsByTagName("*");
             Map books = new LinkedHashMap();
+            //Map elementMap = new LinkedHashMap();
 
             for(int i = 0; i < list.getLength(); i++) {
                 Node nList = list.item(i);
                 Element eElement = (Element) nList;
-                if (nList.getNodeType() == Node.ELEMENT_NODE) {
-                    books.put(nList.getNodeName(), eElement.getElementsByTagName(nList.getNodeName()).item(i).getNodeValue());
+                Map elementMap = new LinkedHashMap();
+
+               for(int j=0; j < nList.getAttributes().getLength(); j++){
+
+                    elementMap.put(nList.getAttributes().item(j).getNodeName(), nList.getAttributes().item(j).getNodeValue());
+                    books.put(nList.getNodeName(),elementMap);
+
                 }
+                //books.put(nList.getNodeName(),elementMap);
             }
 
-            System.out.println(books);
+            //System.out.println(books);
+            //System.out.println(elementMap);
             jsonObject.put(rootNode,books);
+            System.out.println(jsonObject);
+
 
 
 //            PrintWriter pw = new PrintWriter("src/main/java/new_file3.json");
